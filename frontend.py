593f75aead1331e7844d4d2c86a1d3ae7ad1d945
vishwam0101 <vishwamtiwari06@gmail.com>
2025-04-29 17:00:27 +0530

    Add files via upload

---
+++ frontend.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from utility import dialogpt
+
+st.title("Agentic RAG Chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("Ask a question from AgenticRAG paper..."):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = dialogpt.run(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
